d3cd3f8211b0d834567cab47ac927c4c95c0b2a5
Metin Cem Doğan <117816204+metincd@users.noreply.github.com>
2023-11-24 13:17:03 +0300

    profile model added

--- yemek_tarifleri/yemek_tarifleri/models.py
+++ yemek_tarifleri/yemek_tarifleri/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
+    
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     
