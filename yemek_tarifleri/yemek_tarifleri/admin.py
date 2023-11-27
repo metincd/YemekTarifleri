843013f01f48e7dc73ba5ed3f9080e6f3c9e71e3
Metin Cem Doğan <117816204+metincd@users.noreply.github.com>
2023-11-27 17:18:48 +0300

    detailed ingredients

--- yemek_tarifleri/yemek_tarifleri/admin.py
+++ yemek_tarifleri/yemek_tarifleri/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from .models import Profile, Recipe, Comment, Rating
+from .models import Profile, Recipe, Ingredient, Comment, Rating
 
+class IngredientInline(admin.TabularInline):
+    model = Ingredient
+    extra = 1
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [IngredientInline]
+    list_display = ('title', 'created_by', 'created_at')
+    search_fields = ['title', 'instructions']
+    
 admin.site.register(Profile)
-admin.site.register(Recipe)
+admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Comment)
 admin.site.register(Rating)
