9df0a0cde83cbfe4b8a958b9215e07c430599c26
Metin Cem Doğan <117816204+metincd@users.noreply.github.com>
2023-11-27 16:39:29 +0300

    postgres installed

--- yemek_tarifleri/yemek_tarifleri/settings.py
+++ yemek_tarifleri/yemek_tarifleri/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'yemek_tarifleri.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'metin',
+        'USER': 'postgres',
+        'PASSWORD': 'metin123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
