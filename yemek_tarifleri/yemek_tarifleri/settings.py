eb15421d0b5fa84305ff578fa1a555b3b1c178ab
Metin Cem Doğan <117816204+metincd@users.noreply.github.com>
2023-11-27 14:58:26 +0300

    postgres activation attempt

--- yemek_tarifleri/yemek_tarifleri/settings.py
+++ yemek_tarifleri/yemek_tarifleri/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'yemek_tarifleri.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'metin', 
+        'USER': 'metin',
+        'PASSWORD': 'metin123',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
