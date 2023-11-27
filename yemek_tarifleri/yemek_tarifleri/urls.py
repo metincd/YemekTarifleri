ea831d9ce4fefdcc52ce823408fc0c8870f5f9b7
Metin Cem Doğan <117816204+metincd@users.noreply.github.com>
2023-11-27 10:42:54 +0300

    gereksiz yerler kaldirildi

--- yemek_tarifleri/yemek_tarifleri/urls.py
+++ yemek_tarifleri/yemek_tarifleri/urls.py
@@ -16,9 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from website.views import welcome
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('welcome', welcome)
 ]
