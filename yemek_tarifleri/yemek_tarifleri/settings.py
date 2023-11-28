57da1a3ff0bb97734dd5a00719ed8e8b3e1f2364
Metin Cem Doğan <117816204+metincd@users.noreply.github.com>
2023-11-28 17:31:42 +0300

    main pages added

--- yemek_tarifleri/yemek_tarifleri/settings.py
+++ yemek_tarifleri/yemek_tarifleri/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from django.conf import settings
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,6 +45,7 @@ ROOT_URLCONF = 'yemek_tarifleri.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -110,6 +113,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '/static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
