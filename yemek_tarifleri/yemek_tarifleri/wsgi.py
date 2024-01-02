51af59af5b022d2ac6dd2ef27ae54a2b10a475fb
Metin Cem Doğan <117816204+metincd@users.noreply.github.com>
2024-01-02 17:40:01 +0300

    wsgi.py update

--- yemek_tarifleri/yemek_tarifleri/wsgi.py
+++ yemek_tarifleri/yemek_tarifleri/wsgi.py
@@ -1,6 +1,9 @@
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/home/runner/work/yemek_tarifleri/yemek_tarifleri')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yemek_tarifleri.settings')
 
